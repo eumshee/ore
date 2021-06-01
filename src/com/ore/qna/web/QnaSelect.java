f3d7d0a51c61983df18897c5263901fd22f55997
eumshee <esh_317@naver.com>
2021-06-01 09:00:59 +0900

    추가

--- src/com/ore/qna/web/QnaSelect.java
+++ src/com/ore/qna/web/QnaSelect.java
@@ -2,15 +2,31 @@ package com.ore.qna.web;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.ore.common.DbCommand;
+import com.ore.qna.service.QnaService;
+import com.ore.qna.serviceImpl.QnaServiceImpl;
+import com.ore.qna.vo.QnaVO;
 
 public class QnaSelect implements DbCommand {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) {
-		// TODO Auto-generated method stub
-		return null;
+		HttpSession session = request.getSession();
+		String writer = (String) session.getAttribute("id");
+		String id = request.getParameter("id");		
+
+		QnaVO vo = new QnaVO();
+		vo.setWriter(writer);
+		vo.setId(Integer.parseInt(id));
+		
+		QnaService service = new QnaServiceImpl();
+		QnaVO rvo = service.qnaSelect(vo);
+		
+		request.setAttribute("qna", rvo);
+		
+		return "qna/qna.tiles";
 	}
 
 }
